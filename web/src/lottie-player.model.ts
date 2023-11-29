1a60216cddb858c239a9de21021f123f7343a3d4
Jinny You <jinny@lottiefiles.com>
2023-11-29 12:09:27 +0900

    player/web: draft all interfaces

--- web/src/lottie-player.model.ts
+++ web/src/lottie-player.model.ts
@@ -2,78 +2,163 @@ import { LitElement } from 'lit';
 import { property } from 'lit/decorators.js';
 
 // Define library version
-interface LibraryVersion {
+export interface LibraryVersion {
   THORVG_VERSION: string
 }
 
 // Define file type which can be exported
-enum ExportableType {
+export enum ExportableType {
   GIF = 'gif',
 }
 
-export abstract class LottiePlayerModel extends LitElement {
-  @property({ type: Boolean })
-  public direction: number = 1;
+// Define valid player states
+export enum PlayerState {
+  Destroyed = "destroyed",
+  Error = "error",
+  Frozen = "frozen",
+  Loading = "loading",
+  Paused = "paused",
+  Playing = "playing",
+  Stopped = "stopped",
+}
 
+// Define play modes
+export enum PlayMode {
+  Bounce = "bounce",
+  Normal = "normal",
+}
+
+// Define player events
+export enum PlayerEvent {
+	Complete = "complete",
+  Destroyed = "destroyed",
+  Error = "error",
+  Frame = "frame",
+  Freeze = "freeze",
+  Load = "load",
+  Loop = "loop",
+  Pause = "pause",
+  Play = "play",
+  Ready = "ready",
+  Rendered = "rendered",
+  Stop = "stop",
+}
+
+export abstract class LottiePlayerModel extends LitElement {
+  /**
+   * LottieFiles JSON data or URL to JSON.
+   */
+  @property({ type: String })
+  public src?: string;
+
+  /**
+   * Animation speed.
+   */
   @property({ type: Number })
   public speed: number = 1.0;
 
-  @property({ type: Number })
-  public totalFrame?: number;
+  /**
+   * Autoplay animation on load.
+   */
+  @property({ type: Boolean })
+  public autoPlay: boolean = false;
 
+  /**
+   * Number of times to loop animation.
+   */
   @property({ type: Number })
-  public currentFrame?: number;
+  public count?: number;
 
+  /**
+   * Whether to loop animation.
+   */
   @property({ type: Boolean })
   public loop: boolean = false;
 
   /**
+   * Direction of animation.
+   */
+  @property({ type: Number })
+  public direction: number = 1;
+
+  /**
+   * Play mode.
+   */
+  @property()
+  public mode: PlayMode = PlayMode.Normal;
+
+  /**
+   * Intermission
+   */
+  @property()
+  public intermission: number = 1;
+
+  /**
+   * total frame of current animation (readonly)
+   */
+  @property({ type: Number })
+  public readonly totalFrame?: number;
+
+  /**
+   * current frame of current animation (readonly)
+   */
+  @property({ type: Number })
+  public readonly currentFrame?: number;
+
+  // @ts-expect-error: C++ object from WASM (Singleton)
+  private static _tvg?: any;
+
+  // private _prevState?: any;
+
+  // private _counter: number = 1;
+
+  /**
+   * Configure and load
+   */
+  public abstract load (src: string | object): void;
+
+  /**
    * Start playing animation.
    */
-  public abstract play (): void
+  public abstract play (): void;
 
   /**
    * Pause animation.
    */
-  public abstract pause (): void
+  public abstract pause (): void;
 
   /**
    * Stop animation.
    */
-  public abstract stop (): void
-
-  /**
-   * Start playing animation.
-   */
-  public abstract load (src: string | object): void
+  public abstract stop (): void;
 
   /**
    * Seek to a given frame
    *
    * @param frame Frame number to move
    */
-  public abstract seek (frame: number): void
+  public abstract seek (frame: number): void;
 
   /**
    * Sets the repeating of the animation.
    *
    * @param value Whether to enable repeating. Boolean true enables repeating.
    */
-  public abstract setLooping (value: boolean): void
+  public abstract setLooping (value: boolean): void;
 
   /**
    * Animation play direction.
    *
    * @param value Direction values.
    */
-  public abstract setDirection (value: number): void
+  public abstract setDirection (value: number): void;
 
   /**
    * Sets animation play speed.
    *
    * @param value Playback speed.
    */
-  public abstract setSpeed (value: number): void
+  public abstract setSpeed (value: number): void;
 
   /**
    * Set a background color.
@@ -82,17 +167,17 @@ export abstract class LottiePlayerModel extends LitElement {
    *
    * @param value Background color(ff00ff00)
    */
-  public abstract setBgColor (value: number): void
+  public abstract setBgColor (value: number): void;
 
   /**
    * Save current animation to other file type
    *
    * @param target File type to export
    */
-  public abstract save (target: ExportableType): void
+  public abstract save (target: ExportableType): void;
 
   /**
    * Returns thorvg version
    */
-  public abstract getVersions (): LibraryVersion
+  public abstract getVersions (): LibraryVersion;
 }
