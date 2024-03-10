439f0cdaca4eefb41bdff3e5a36d847321d3463f
Jinny You <jinny@lottiefiles.com>
2024-03-10 19:33:16 +0900

    flutter: Reduced binary size
    Removed `savers` and unnecessary `loaders`, only enabled lottie, jpg and png.
    
    outputs:
    - android(arm, x86) : 2.1MB for each (-19%)
    - ios(arm, x86-sim): 479KB for each (-23%)

--- flutter_build.ios.sh
+++ flutter_build.ios.sh
@@ -2,14 +2,14 @@
 
 if [ ! -d "./build_flutter_aarch64" ]; then
     sed "s|EMSDK:|$1|g" ./cross/flutter_ios_aarch64.txt > /tmp/.flutter_cross.txt
-    meson -Db_lto=true -Ddefault_library=static -Dloaders="all" -Dsavers="all" -Dbindings="flutter_beta" --cross-file /tmp/.flutter_cross.txt build_flutter_aarch64
+    meson -Db_lto=true -Ddefault_library=static -Dloaders="lottie, png, jpg" -Dbindings="flutter_beta" --cross-file /tmp/.flutter_cross.txt build_flutter_aarch64
 fi
 
 ninja -C build_flutter_aarch64/
 
 if [ ! -d "./build_flutter_x86_64" ]; then
     sed "s|EMSDK:|$1|g" ./cross/flutter_ios_x86_64.txt > /tmp/.flutter_cross.txt
-    meson -Db_lto=true -Ddefault_library=static -Dloaders="all" -Dsavers="all" -Dbindings="flutter_beta" --cross-file /tmp/.flutter_cross.txt build_flutter_x86_64
+    meson -Db_lto=true -Ddefault_library=static -Dloaders="lottie, png, jpg" -Dbindings="flutter_beta" --cross-file /tmp/.flutter_cross.txt build_flutter_x86_64
 fi
 
 ninja -C build_flutter_x86_64/
