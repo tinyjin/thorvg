4f25abb8cbc9e55b71ffb94b9e806be46ecf04d5
Jinny You <jinny@lottiefiles.com>
2024-03-16 23:53:17 +0900

    flutter: Support arm64 binary for iOS simulator
    - libsimthorvg.dylib : simulator binary supports both arm64 & x86_64
    - libthorvg.dylib : real device binary

--- flutter_build.ios.sh
+++ flutter_build.ios.sh
@@ -7,12 +7,26 @@ fi
 
 ninja -C build_flutter_aarch64/
 
+if [ ! -d "./build_flutter_aarch64_iossim" ]; then
+    sed "s|EMSDK:|$1|g" ./cross/flutter_ios_simulator_aarch64.txt > /tmp/.flutter_cross.txt
+    meson -Db_lto=true -Ddefault_library=static -Dloaders="lottie, png, jpg" -Dbindings="flutter_beta" --cross-file /tmp/.flutter_cross.txt build_flutter_aarch64_iossim
+fi
+
+ninja -C build_flutter_aarch64_iossim/
+
 if [ ! -d "./build_flutter_x86_64" ]; then
-    sed "s|EMSDK:|$1|g" ./cross/flutter_ios_x86_64.txt > /tmp/.flutter_cross.txt
+    sed "s|EMSDK:|$1|g" ./cross/flutter_ios_simulator_x86_64.txt > /tmp/.flutter_cross.txt
     meson -Db_lto=true -Ddefault_library=static -Dloaders="lottie, png, jpg" -Dbindings="flutter_beta" --cross-file /tmp/.flutter_cross.txt build_flutter_x86_64
 fi
 
 ninja -C build_flutter_x86_64/
 
-ls -lrt build_flutter_aarch64/src/bindings/flutter/libthorvg.dylib
-ls -lrt build_flutter_x86_64/src/bindings/flutter/libthorvg.dylib
+mkdir build_flutter_ios
+cp build_flutter_aarch64/src/bindings/flutter/libthorvg.dylib ./build_flutter_ios
+
+lipo build_flutter_aarch64_iossim/src/bindings/flutter/libthorvg.dylib \
+build_flutter_x86_64/src/bindings/flutter/libthorvg.dylib \
+-output build_flutter_ios/libsimthorvg.dylib -create
+
+ls -lrt build_flutter_ios/libthorvg.dylib
+ls -lrt build_flutter_ios/libsimthorvg.dylib
