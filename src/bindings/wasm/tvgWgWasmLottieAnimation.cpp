ab30ca305eebdb1cf716c63fc8c4a9c82adfbca6
Sergii Liebodkin <s.lebedkin@gmail.com>
2024-07-28 18:13:58 +0000

    wg_engine: emscripten and browser support [issues 2410: emscripten support](https://github.com/thorvg/thorvg/issues/2410) - Research and prototype the Emscripten build with WebGPU.
    meson setup script:
       meson setup builddir -Ddefault_library=static -Dloaders=all -Dsavers="all" -Dbindings="wasm_beta" -Dengines="wg_beta" --cross-file ./cross/wasm_webgpu.txt

---
+++ src/bindings/wasm/tvgWgWasmLottieAnimation.cpp
@@ -0,0 +1,156 @@
+/*
+ * Copyright (c) 2020 - 2024 the ThorVG project. All rights reserved.
+
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#include <emscripten/bind.h>
+#include <webgpu/webgpu.h>
+#include <thorvg.h>
+#include <iostream>
+
+class TvgWgLottieAnimation
+{
+public:
+    TvgWgLottieAnimation()
+    { 
+        tvg::Initializer::init(0, tvg::CanvasEngine::Wg);
+        canvas = tvg::WgCanvas::gen();
+    }
+
+    ~TvgWgLottieAnimation()
+    {
+        wgpuSurfaceRelease(surface);
+        wgpuInstanceRelease(instance);
+        tvg::Initializer::term();
+     }
+
+    void resize(uint32_t width, uint32_t height)
+    {
+        // instance
+        instance = wgpuCreateInstance(nullptr);
+
+        // surface
+        WGPUSurfaceDescriptorFromCanvasHTMLSelector canvasDesc{};
+        canvasDesc.chain.next = nullptr;
+        canvasDesc.chain.sType = WGPUSType_SurfaceDescriptorFromCanvasHTMLSelector;
+        canvasDesc.selector = "#canvasWebgpu";
+        WGPUSurfaceDescriptor surfaceDesc{};
+        surfaceDesc.nextInChain = &canvasDesc.chain;
+        surface = wgpuInstanceCreateSurface(instance, &surfaceDesc);
+
+        // store size
+        this->width = width;
+        this->height = height;
+
+        // init canvas target
+        canvas->target(this->instance, this->surface, width, height);
+    }
+
+    void render()
+    {
+        if (canvas->draw() == tvg::Result::Success) {
+            canvas->sync();
+        }
+    }
+
+    bool frame(float no)
+    {
+        if(!animation) return false;
+        animation->frame(no);
+        return true;
+    }
+
+    bool update()
+    {
+        canvas->update();
+        return true;
+    }
+
+    float duration()
+    {
+        if (!canvas || !animation) return 0;
+        return animation->duration();
+    }
+
+    float totalFrame()
+    {
+        if (!canvas || !animation) return 0;
+        return animation->totalFrame();
+    }
+
+    bool load(std::string data, std::string mimetype)
+    {
+        // clear canvas
+        canvas->clear(true);
+
+        // create new animation
+        animation = tvg::Animation::gen();
+
+        // load data to animation
+        if (animation->picture()->load(data.c_str(), data.size(), mimetype) != tvg::Result::Success) {
+            std::cout << "animation->picture()->load(...) failed" << std::endl;
+            return false;
+        }
+
+        float w{}, h{};
+        animation->picture()->size(&w, &h);
+
+        float scale{};
+        float shiftX = 0.0f, shiftY = 0.0f;
+        if (w > h) {
+            scale = width / w;
+            shiftY = (height - h * scale) * 0.5f;
+        } else {
+            scale = height / h;
+            shiftX = (width - w * scale) * 0.5f;
+        }
+        animation->picture()->scale(scale);
+        animation->picture()->translate(shiftX, shiftY);
+
+        // push animation to canvas
+        if (canvas->push(cast(animation->picture())) != tvg::Result::Success) {
+            std::cout << "canvas->push(cast(animation->picture())) failed" << std::endl;
+            return false;
+        }
+
+        return true;
+    }
+
+private:
+    WGPUInstance instance{};
+    WGPUSurface surface{};
+    uint32_t width{};
+    uint32_t height{};
+
+    std::unique_ptr<tvg::WgCanvas> canvas{};
+    std::unique_ptr<tvg::Animation> animation{};
+};
+
+EMSCRIPTEN_BINDINGS(thorvg_bindings) {
+    emscripten::class_<TvgWgLottieAnimation>("TvgWgLottieAnimation")
+        .constructor()
+        .function("resize", &TvgWgLottieAnimation::resize)
+        .function("render", &TvgWgLottieAnimation::render)
+        .function("frame", &TvgWgLottieAnimation::frame)
+        .function("update", &TvgWgLottieAnimation::update)
+        .function("duration", &TvgWgLottieAnimation::duration)
+        .function("totalFrame", &TvgWgLottieAnimation::totalFrame)
+        .function("load", &TvgWgLottieAnimation::load);
+}
--- src/renderer/wg_engine/tvgWgCommon.cpp
+++ src/renderer/wg_engine/tvgWgCommon.cpp
@@ -21,6 +21,9 @@
  */
 
 #include "tvgWgCommon.h"
+#ifdef __EMSCRIPTEN__
+#include <emscripten.h>
+#endif
 #include <iostream>
 
 //*****************************************************************************
@@ -50,12 +53,12 @@ void WgContext::initialize(WGPUInstance instance, WGPUSurface surface)
     };
     // request adapter
     wgpuInstanceRequestAdapter(instance, &requestAdapterOptions, onAdapterRequestEnded, &adapter);
-    assert(adapter);
+    #ifdef __EMSCRIPTEN__
+    while (!adapter) emscripten_sleep(10);
+    #endif
 
-    // adapter enumerate features
+    // get feature names
     size_t featuresCount = wgpuAdapterEnumerateFeatures(adapter, featureNames);
-    wgpuAdapterGetProperties(adapter, &adapterProperties);
-    wgpuAdapterGetLimits(adapter, &supportedLimits);
 
     // request device
     WGPUDeviceDescriptor deviceDesc{};
@@ -76,7 +79,9 @@ void WgContext::initialize(WGPUInstance instance, WGPUSurface surface)
     };
     // request device
     wgpuAdapterRequestDevice(adapter, &deviceDesc, onDeviceRequestEnded, &device);
-    assert(device);
+    #ifdef __EMSCRIPTEN__
+    while (!device) emscripten_sleep(10);
+    #endif
 
     // on device error function
     auto onDeviceError = [](WGPUErrorType type, char const* message, void* pUserData) {
@@ -535,8 +540,10 @@ WGPUShaderModule WgPipeline::createShaderModule(WGPUDevice device, const char* c
     WGPUShaderModuleDescriptor shaderModuleDesc{};
     shaderModuleDesc.nextInChain = &shaderModuleWGSLDesc.chain;
     shaderModuleDesc.label = label;
+    #ifndef __EMSCRIPTEN__
     shaderModuleDesc.hintCount = 0;
     shaderModuleDesc.hints = nullptr;
+    #endif
     return wgpuDeviceCreateShaderModule(device, &shaderModuleDesc);
 }
 
